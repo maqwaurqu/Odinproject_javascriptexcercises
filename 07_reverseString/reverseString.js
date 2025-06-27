c7b466f5a7e5adf7693aef879d33e4a85aaf0541
maqwaurqu <207817625+maqwaurqu@users.noreply.github.com>
2025-06-27 08:21:55 +0000

    solved 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let b=''
+    string=String(string)
+    for (let i =string.length-1;i>=0;i--) {
+        let a=[]
+        a[i]=string[i]
+        b+=a[i]
+    }
+    return b 
 };
 
 // Do not edit below this line

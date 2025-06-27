09466f9a8f5d4e7a969f6795864a3dfbfaab36bc
maqwaurqu <207817625+maqwaurqu@users.noreply.github.com>
2025-06-27 07:56:05 +0000

    solved 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    if (num>=0){
+    let a =''
+    for (let i =0;i<num;i++){
+        a += string
+        
+    }
+    return a
+}
+
+else {
+    return "ERROR"
+}
 
 };
 
+repeatString('hey',3)
 // Do not edit below this line
 module.exports = repeatString;

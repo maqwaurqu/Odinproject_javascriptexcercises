db17bd8871d00e8d4abdd833ce6fece367d58485
maqwaurqu <207817625+maqwaurqu@users.noreply.github.com>
2025-06-28 07:01:39 +0000

    solved 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+temperature=(temperature-32)*(5/9)
+temperature=Number(temperature.toFixed(1))
+return temperature
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+temperature=(temperature*9/5)+32
+temperature=Number(temperature.toFixed(1))
+return temperature
 };
 
 // Do not edit below this line

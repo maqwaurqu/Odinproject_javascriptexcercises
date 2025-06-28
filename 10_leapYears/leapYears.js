cdcbf44d9690be42464948e9353ac4ff1936bcb3
maqwaurqu <207817625+maqwaurqu@users.noreply.github.com>
2025-06-28 06:09:24 +0000

    solved10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    if (!(years%4)) {
+        if (!(years%400)) {
+            return true
+        }
+        else if ((years%400)&&(years%100)) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line

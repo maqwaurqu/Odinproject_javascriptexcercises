05122839bd049503166227ccffd3409d10b04ee1
maqwaurqu <207817625+maqwaurqu@users.noreply.github.com>
2025-07-13 07:34:28 +0000

    solved 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+member=Number(member)
+let a0=0
+let a1=1
+let term=1
+if (member>=0){if (member==0) {
+    return member
+}
+else {for (let t=1;t<member;t++){
+    term=a1+a0
+    a0=a1
+    a1=term
+}
+return term}}
+else{
+    return 'OOPS'
+}
 };
 
 // Do not edit below this line

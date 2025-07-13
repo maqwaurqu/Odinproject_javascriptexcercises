b5f5d3ebea8d41ea53e6c37d8447130da0711ec1
maqwaurqu <207817625+maqwaurqu@users.noreply.github.com>
2025-07-13 07:41:50 +0000

    solved 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+let a=[]
+for (let i =0;(i<books.length);i++) {
+a[i]=books[i].title
+}
+return a
 };
 
 // Do not edit below this line
